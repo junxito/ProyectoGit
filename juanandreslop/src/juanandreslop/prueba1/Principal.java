170ac98da4c15df4c6413a4c5d343beaf133c42d
junxito <juanandreslopezjimenez@gmail.com>
2022-03-30 09:54:03 +0200

    Hacemos commit del nuevo cambio a Principal

--- juanandreslop/src/juanandreslop/prueba1/Principal.java
+++ juanandreslop/src/juanandreslop/prueba1/Principal.java
@@ -5,6 +5,7 @@ public class Principal {
 	public static void main(String[] args) {
 		System.out.println("Hola Mundo java-git");
 		System.out.println("Añade segunda línea. Paso 2.");
+		System.out.println("Añado linea en la rama cambio1");
 	}
 
 }
